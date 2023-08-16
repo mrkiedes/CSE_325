96b0a443678bb4f17560540804f1c631d59549c1
Aaron Kiedes <akiedes@vtsd.com>
2023-08-16 15:40:41 -0400

    Team 2 Completed

--- team2.py
+++ team2.py
@@ -7,31 +7,33 @@ import random
 
 strategy_name = "Historical Choices"
 
-def basic_move(their_history):
-    match their_history[len(their_history) - 1]:
-        case "r":
-            pick = "p"
-        case "p":
-            pick = "s"
-        case "s":
-            pick = "r"
+
+
+
+def basic_move(move):
+    if move == "r":
+        pick = "p"
+    if move == "p":
+        pick = "s"
+    if move == "s":
+        pick = "r"
 
 def historical_move(their_history):
     temp = 1
 
 def move(my_history, their_history):
-    # Default case
-    pick = random.choice(["r", "p", "s"])
-    # If its my first move then move randomly
-    print("Length of their history " + str(len(their_history)))
+    # Default case - used to make sure there is always a pick.  This gets changed throughout the code.
+    pick = "r"
 
-    if len(my_history) <= 1:
+    if len(my_history) <= 0:
+        pick = random.choice(["r", "p", "s"])
+
+    elif len(my_history) == 1:
         # If I have moved once, then base my next move on their history
-        print("1 Case")
-        basic_move(their_history)
+        basic_move(their_history[-1])
     else:
         # If we are on the second or further, look for a pattern with the last two moves
-        # If a pattern is found, counterpick it.  Otherwise choose randomly.
+        # If a pattern is found, counterpick it.  Otherwise, choose randomly.
         immediate_history = their_history[len(their_history) - 1] + their_history[len(their_history) - 2]
         if immediate_history[-2] == immediate_history[-1]:
             if immediate_history[-2] == "r":
